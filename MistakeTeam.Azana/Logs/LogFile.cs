97a8b047a35c377ab5e176e36067969e160c73d6
xDeltaFox <israelbz25@gmail.com>
2022-12-05 17:28:53 -0300

    Sistema de log

---
+++ MistakeTeam.Azana/Logs/LogFile.cs
@@ -0,0 +1,59 @@
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace MistakeTeam.Azana.Logs
+{
+    public static class LogFile
+    {
+        private static bool _iniciado = false;
+        private static string _filepath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\logs";
+        private static string _filename = string.Format("\\azana_log_{0}-{1}-{2}T{3}-{4}-{5}.txt",
+                    DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year, DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+
+        public static void Iniciar()
+        {
+            if (!_iniciado)
+            {
+                if (!Directory.Exists(_filepath))
+                {
+                    Directory.CreateDirectory(_filepath);
+                }
+
+                using StreamWriter _w = File.AppendText(_filepath + _filename);
+                _w.Close();
+            }
+
+            _iniciado = true;
+        }
+
+        public static void WriteLine(string mensagem)
+        {
+            try
+            {
+                using StreamWriter _filelog = File.AppendText(_filepath + _filename);
+
+                StackTrace st = new StackTrace();
+                StackFrame sf = st.GetFrame(1);
+
+                if (sf.GetMethod().Name == "WriteLine")
+                {
+                    sf = st.GetFrame(2);
+                }
+
+                _filelog.Write("\r\n[{0} {1}] ", DateTime.Now.ToShortDateString(), DateTime.Now.ToLongTimeString());
+                _filelog.Write("[{0}.{1}:{2}] {3}",
+                    sf.GetMethod().DeclaringType.Namespace, sf.GetMethod().DeclaringType.Name, sf.GetMethod().Name, mensagem);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public static void WriteLine(string mensagem, params object?[] args)
+        {
+            WriteLine(string.Format(mensagem, args));
+        }
+    }
+}
