75808796866f590f892d5bc008288be12ab6a4b1
xDeltaFox <israelbz25@gmail.com>
2022-12-05 17:29:23 -0300

    Pequenas alterações

--- MistakeTeam.Azana/Texto/Localizar.cs
+++ MistakeTeam.Azana/Texto/Localizar.cs
@@ -1,28 +1,32 @@
 using System;
-using System.Resources;
+using System.Globalization;
 using System.Reflection;
+using System.Resources;
 
-namespace MistakeTeam.Azana.Texto {
+namespace MistakeTeam.Azana.Texto
+{
     ///<Summary>
     /// Localizar
     ///</Summary>
-    public class Localizar {
+    public class Localizar
+    {
         ///<Summary>
         /// Entra no resources de textos e retorna uma string com o valor da chave passada nos parâmetros.
         ///</Summary>
-        public static string PegarTexto(string bloco, string chave) {
+        public static string PegarTexto(string bloco, string chave)
+        {
             string lang = "PT-BR";
             string pathEXE = Assembly.GetExecutingAssembly().Location.Replace("Azana.dll", "");
-            string resourcePath = Path.GetFullPath(pathEXE + $"/resources/{lang}/{bloco}.resources");
-            
+            string resourcePath = Path.GetFullPath(
+                pathEXE + $"/resources/{lang}/{bloco}.resources"
+            );
+
             //ConsoleLine.Enviar(resourcePath);
-            ResourceReader rr = new ResourceReader(resourcePath);
+            ResourceReader rr = new(resourcePath);
 
-            string dataType = null;
-            byte[] data = null;
-            rr.GetResourceData(chave, out dataType, out data);
+            rr.GetResourceData(chave, out string dataType, out byte[] data);
 
-            BinaryReader reader = new BinaryReader(new MemoryStream(data));
+            BinaryReader reader = new(new MemoryStream(data));
             string binData = reader.ReadString();
 
             rr.Close();
