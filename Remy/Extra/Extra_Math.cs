eb4da8984d459f40a4ac42d2c3e9752047dc7c22
xDeltaFox <Israelbz25@gmail.com>
2023-11-20 18:39:00 +0000

    ✨ | Extra Math 🔢

---
+++ Remy/Extra/Extra_Math.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Remy.Extra
+{
+    ///<Summary>
+    /// Classe calculadora, concentrando todas as funções matemáticas.
+    ///</Summary>
+    public static class Extra_Math
+    {
+        ///<Summary>
+        /// Pegar a porcentagem de um valor.
+        ///</Summary>
+        public static float PegarPorcentagem(float valor, float total)
+        {
+            return (valor / total) * 100;
+        }
+
+        public static float PegarValorPorcentagem(float porcento, float total)
+        {
+            if(porcento>100) porcento = 100;
+            return (total / porcento) * 100;
+        }
+    }
+}
